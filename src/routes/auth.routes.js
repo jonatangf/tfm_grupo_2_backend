d48177f4c18eee0f24be906b9e5fd50e9e511361
Antony Martinez <antony11995@hotmail.com>
2025-11-13 04:03:32 +0100

    Adding curly bracets to asyncHandler over auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const { registerValidation, loginValidation } = require("../validations/auth.validation");
-const asyncHandler = require("../middlewares/asyncHandler");
+const { asyncHandler } = require("../middlewares/asyncHandler");
 
 router.post("/register", registerValidation, asyncHandler(authController.register));
 router.post("/login", loginValidation, asyncHandler(authController.login));
